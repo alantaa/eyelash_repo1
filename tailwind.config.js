37055b1eba0506165aaa64ec60bd6e72ccb7aa9b
Alanta Tarling <alanta997@gmail.com>
2022-08-30 00:14:36 +0100

    Blob color animation implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
       philosopher: ["Philosopher", "sans-serif"],
       poppins: ["Poppins", "sans-serif"],
     },
-    extends: {},
+    extend: {
+      animation: {
+        blob: "blob 4s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "33%": {
+            transform: "scale(1.1)",
+          },
+          "66%": {
+            transform: "scale(0.9)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+    },
+    plugins: [require("tailwindcss"), require("autoprefixer")],
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
