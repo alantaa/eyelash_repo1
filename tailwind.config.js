fa862a82e75b33db385b4544b254a4f9ad1ad9e1
Alanta Tarling <alanta997@gmail.com>
2022-08-31 22:46:44 +0100

    Progressing with making nav and hero responsive in TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,7 @@ module.exports = {
       philosopher: ["Philosopher", "sans-serif"],
       poppins: ["Poppins", "sans-serif"],
     },
-    extend: {
-      animation: {
-        blob: "blob 4s infinite",
-      },
-      keyframes: {
-        blob: {
-          "0%": {
-            transform: "scale(1)",
-          },
-          "33%": {
-            transform: "scale(1.1)",
-          },
-          "66%": {
-            transform: "scale(0.9)",
-          },
-          "100%": {
-            transform: "scale(1)",
-          },
-        },
-      },
-    },
+    extend: {},
     plugins: [require("tailwindcss"), require("autoprefixer")],
   },
 };
